a49ab2dfbf15a3891d16d84aef3fe39a5ae4dade
Valeriya Subocheva <subocheva.valeria@gmail.com>
2022-10-25 23:34:22 +0300

    type URLInfo

--- types.go
+++ types.go
@@ -3,9 +3,8 @@ package urls
 import "time"
 
 type UrlInfo struct {
-	//Id          int       `json:"id" db:"id"`
-	OriginalURL string    `json:"original_URL" db:"original_URL"`
-	ShortURL    string    `json:"short_URL" db:"short_URL"`
-	CreatedAt   time.Time `json:"created_at" db:"created_at"`
-	Visits      int       `json:"visits" db:"visits"`
+	OriginalURL string    `json:"original_URL" db:"original_URL" bson:"original_URL"`
+	ShortURL    string    `json:"short_URL" db:"short_URL" bson:"short_URL"`
+	CreatedAt   time.Time `json:"created_at" db:"created_at" bson:"created_at"`
+	Visits      int       `json:"visits" db:"visits" bson:"visits"`
 }
